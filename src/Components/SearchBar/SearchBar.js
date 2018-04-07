c1bc60bab571c33cf29cee7808181b14811953cd
Adnan <aadnan@USAMAC48KHF1P.local>
2018-04-07 17:47:57 -0500

    enter button initializes search

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -2,29 +2,47 @@ import React from 'react';
 import './SearchBar.css';
 
 class SearchBar extends React.Component {
-    constructor(props){
-      super(props);
+  constructor(props) {
+    super(props);
 
-      this.search = this.search.bind(this);
-      this.handleTermChange = this.handleTermChange.bind(this);
-    }
+    this.search = this.search.bind(this);
+    this.handleTermChange = this.handleTermChange.bind(this);
+    this.handleKeyPress = this.handleKeyPress.bind(this);
+  }
 
-    search() {
+  search() {
+    this.props.onSearch(this.state.term);
+  }
+  handleKeyPress(event) {
+    if (event.keyCode === 13) {
       this.props.onSearch(this.state.term);
     }
+  }
 
-    handleTermChange(event) {
-      this.setState({term: event.target.value});
-    }
+  handleTermChange(event) {
+    this.setState({
+      term: event.target.value
+    });
+  }
 
-    render() {
-      return (
-        <div className="SearchBar">
-          <input placeholder="Enter A Song, Album, or Artist" onChange={this.handleTermChange}/>
-          <a onClick={this.search}>SEARCH</a>
-        </div>
-      );
-    }
+  render() {
+    return ( <
+      div className = "SearchBar" >
+      <
+      input onKeyDown = {
+        this.handleKeyPress
+      }
+      placeholder = "Enter A Song, Album, or Artist"
+      onChange = {
+        this.handleTermChange
+      }
+      /> <
+      a onClick = {
+        this.search
+      } > SEARCH < /a> < /
+      div >
+    );
+  }
 }
 
 export default SearchBar;
